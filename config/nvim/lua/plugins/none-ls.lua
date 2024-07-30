b3ed0cb4d925c1a9d721faf0ee6a9a229c55d93f
Vlad Suciu <suciu.vlad@gmail.com>
2024-07-30 23:08:46 +0300

    fix: remove duplicate sources

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
           null_ls.builtins.diagnostics.golangci_lint,  -- Linting with golangci-lint
           null_ls.builtins.formatting.gofumpt,         -- Formatting with gofumpt
           null_ls.builtins.formatting.goimports,       -- Automatically format imports
-          null_ls.builtins.diagnostics.golangci_lint,  -- Diagnostics with golangci-lint
           null_ls.builtins.code_actions.gomodifytags,  -- Modify struct tags
           null_ls.builtins.code_actions.impl           -- Generate method stubs for implementing an interface
         }
