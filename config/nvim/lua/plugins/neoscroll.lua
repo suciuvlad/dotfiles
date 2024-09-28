2ec90bb820df30338b47e5b9d9d4cf7e447f4a44
Vlad Suciu <suciu.vlad@gmail.com>
2024-09-28 13:48:01 +0300

    fix: set_mappings() is deprecated

--- config/nvim/lua/plugins/neoscroll.lua
+++ config/nvim/lua/plugins/neoscroll.lua
@@ -2,10 +2,13 @@ return {
   {
     'karb94/neoscroll.nvim',
     config = function()
-      require('neoscroll.config').set_mappings {
-        ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '50' } },
-        ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '50' } },
-      }
+      require('neoscroll').setup()
+
+      local scroll = require('neoscroll').scroll
+
+      -- Custom scroll mappings using neoscroll.scroll()
+      vim.keymap.set('n', '<C-u>', function() scroll(-vim.wo.scroll, true, 50) end)
+      vim.keymap.set('n', '<C-d>', function() scroll(vim.wo.scroll, true, 50) end)
     end
   }
 }
